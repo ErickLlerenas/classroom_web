6b56fdca46930e58c6d619b2938aa936efd1390d
Erick <dev.llerenas@gmail.com>
2020-08-20 20:12:26 -0500

    added top navigation bar

--- src/App.js
+++ src/App.js
@@ -5,18 +5,18 @@ import WhatsAppButton from "./components/WhatsAppButton";
 import PendingTasks from "./pages/PendingTasks";
 import DoneTasks from "./pages/DoneTasks";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import BottomNagivationBar from './components/BottonNavigationBar';
+import TopNavigationBar from "./components/TopNavigationBar";
 
 export default function App() {
   return (
     <Router>
+      <TopNavigationBar />
       <Switch>
         <Route exact path="/" component={PendingTasks} />
         <Route exact path="/iniciar-sesion" component={SignIn} />
         <Route exact path="/crear-cuenta" component={SignUp} />
         <Route exact path="/tareas-entregadas" component={DoneTasks} />
       </Switch>
-      <BottomNagivationBar/>
       <WhatsAppButton />
     </Router>
   );
